361460468db77e368fe863eaec18af2df7400067
Mikko Haapoja <me@mikkoh.com>
2015-07-31 13:00:26 -0400

    Licenses are now being export as json to repo folders

--- test/writingLicenses.js
+++ test/writingLicenses.js
@@ -1,12 +1,13 @@
 'use strict';
 
-var getLicenses = require('../lib/getLicenses');
 var getPackageJSON = require('package-json');
+var getLicenses = require('../lib/getLicenses');
+var writeLicenses = require('../lib/writeLicenses');
 
 getPackageJSON('bigwheel', 'latest', function(err, packageJSON) {
 
   getLicenses(packageJSON, Number.MAX_VALUE, function(err, licenses) {
 
-    console.log(JSON.stringify(licenses, null, '  '));
+    writeLicenses( { staticPath: 'static/' }, 'testRepo', licenses);
   });
 });
