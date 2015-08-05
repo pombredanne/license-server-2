6f5ecb431f06027b2eced1cd34a9e07352724465
Mikko Haapoja <me@mikkoh.com>
2015-08-05 10:18:39 -0400

    Implementing passing environment variables via api

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ var pm2 = require('pm2');
  
 pm2.connect(function() {
   pm2.start({
-    script    : 'child.js',
-    exec_mode : 'cluster'
+    script: 'child.js',
+    exec_mode: 'cluster',
+    env: {
+      PORT: process.env.PORT,
+      gitToken: process.env.gitToken,
+      gitHookSecret: process.env.gitHookSecret,
+      org: process.env.org,
+      repoRegex: process.env.repoRegex
+    }
   }, function(err, apps) {
     pm2.disconnect();
   });
